d22392f35b7083386735c6f1a9833aae781bf15c
Harsh Saini <hs7222867@gmail.com>
2024-08-10 23:59:54 +0530

    login page added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { red } from "@mui/material/colors";
+import { Work_Sans } from "next/font/google";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+
+const work_sans = Work_Sans({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // Create a theme instance.
+  const theme = createTheme({
+    palette: {
+      // primary: {
+      //   main: "#556cd6",
+      // },
+      // secondary: {
+      //   main: "#19857b",
+      // },
+      error: {
+        main: red.A400,
+      },
+    },
+    typography: {
+      fontFamily: work_sans.style.fontFamily,
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <main className={work_sans.className}>
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  );
 }
---
+++ src/pages/chat/index.tsx
@@ -0,0 +1,100 @@
+import styles from "./index.module.scss";
+import { IoMdCall } from "react-icons/io";
+import { IoVideocamSharp } from "react-icons/io5";
+import { CiSearch } from "react-icons/ci";
+import { IoMdMore } from "react-icons/io";
+import { MdMessage } from "react-icons/md";
+import { MdGroups2 } from "react-icons/md";
+import { PiCardsThreeDuotone } from "react-icons/pi";
+import { IoFilterOutline } from "react-icons/io5";
+
+export default function Home() {
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <Layout />
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.left}>
+        <div className={styles.avatar}></div>
+        <div className={styles.actions}>
+          <MdGroups2 />
+          <PiCardsThreeDuotone />
+          <MdMessage />
+          <IoMdMore />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.selectedUser}>
+          <div className={styles.avatar}></div>
+          <div className={styles.name}>Harsh Saini</div>
+        </div>
+        <div className={styles.actions}>
+          <IoMdCall />
+          <IoVideocamSharp />
+          <CiSearch />
+          <IoMdMore />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const Layout = () => {
+  const List: any = Array.from({ length: 4 }).map((x, i) => {
+    return {
+      name: "Designers Corner",
+      time: "3:11 pm",
+      count: "197",
+      message:
+        "That’s a great news! Congratulations Congratulations Congratulations Congratulations",
+      fromWhere: "2348128225157",
+      isGroup: i % 2 == 0,
+    };
+  });
+
+  const getChatBox = (item: any) => {
+    return (
+      <li key={`chat-${item.name}`} className={styles.card}>
+        <div className={styles.avatar}></div>
+        <div className={styles.box}>
+          <div className={styles.nameAndTime}>
+            <div className={styles.name}>{item.name}</div>
+            <div className={styles.time}>{item.time}</div>
+          </div>
+          <div className={styles.lowerRow}>
+            <div className={styles.message}>
+              {item?.isGroup && <span>{item.name} :</span>}
+              {item.message}
+            </div>
+            <div className={styles.count}>{item.count}</div>
+          </div>
+        </div>
+      </li>
+    );
+  };
+  return (
+    <main className={styles.layout}>
+      <div className={styles.left}>
+        <div className={styles.filter}>
+          <div className={styles.searchBox}>
+            <input placeholder="Search or start new chat" />
+            <CiSearch />
+          </div>
+          <IoFilterOutline />
+        </div>
+        <ul>
+          {List.map((item: any) => {
+            return getChatBox(item);
+          })}
+        </ul>
+      </div>
+      <div className={styles.right}></div>
+    </main>
+  );
+};
